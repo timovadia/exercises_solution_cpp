9cd0503be02b22c6ce99ee1882d40fe22b440fc8
Timur Berezin <timurovadia@gmail.com>
2020-11-07 22:34:37 +0300

    added new files foo_query_stopwords, foo_set_intersection

---
+++ foo_query_stopwords.cpp
@@ -0,0 +1,61 @@
+﻿//Научите свою поисковую систему отбрасывать стоп-слова.
+//На вход примите сначала строку стоп-слов, а затем с нового абзаца — строку-запрос.
+//
+//Выводите только те слова, которых нет в списке запрещённых.
+//
+//Ввод:
+//with many very
+//very kind dog with brown fur
+//
+//Вывод :
+//[kind]
+//[dog]
+//[brown]
+//[fur]
+
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+set<string> MakeSet(vector<string> query_words) {
+    set<string> s(query_words.begin(), query_words.end());
+    return s;
+}
+
+vector<string> SplitIntoWords(string text) {
+    vector<string> words;
+    string word;
+    for (int i = 0; i < text.size(); ++i) {
+        if (text[i] == ' ') {
+            words.push_back(word);
+            word = "";
+        }
+        else {
+            word += text[i];
+        }
+    }
+    words.push_back(word);
+
+    return words;
+}
+
+int main() {
+    string query, stopwords;
+
+    getline(cin, stopwords);
+    getline(cin, query);
+
+    vector<string> v_query_words = SplitIntoWords(query);
+    vector<string> v_stopwords = SplitIntoWords(stopwords);
+
+    set<string> set_stopwords = MakeSet(v_stopwords);
+
+    for (string word : v_query_words) {
+        if (!set_stopwords.count(word)) {
+            cout << '[' << word << ']' << endl;
+        }
+    }
+}
---
+++ foo_set_intersection.cpp
@@ -0,0 +1,55 @@
+﻿//Представьте, что анализируете запросы пользователей в вашей поисковой системе.Есть слова запроса и описание животного.
+//Посчитайте релевантность запроса — размер множества слов, которые входят и в запрос, и в описание.
+//
+//Формат ввода
+//Сначала вводится строка - запрос, затем с новой строчки строка - описание.
+//
+//Формат вывода
+//Выведите единственное целое число — размер пересечения множеств слов из запроса и описания.
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int CalculateSetIntersection(set<string> first, set<string> second) {
+    //размер пересечения множеств слов
+    int i = 0;
+    for (auto elm : first) {
+        if (second.count(elm)) {
+            i++;
+        }
+    }
+    return i;
+}
+
+set<string> StringToSetWords(string text) {
+    //формируем множество слов
+    set<string> set_words;
+    string word;
+    for (int i = 0; i < text.size(); ++i) {
+        if (text[i] == ' ') {
+            set_words.insert(word);
+            word = "";
+        }
+        else {
+            word += text[i];
+        }
+    }
+    set_words.insert(word);
+
+    return set_words;
+}
+
+int main() {
+    string query, description;
+
+    getline(cin, query);
+    getline(cin, description);
+
+    cout << CalculateSetIntersection(StringToSetWords(query), StringToSetWords(description)) << endl;
+
+    return 0;
+}
