686de872ff22e3d96206e5a880ed84091cfac96b
Timur Berezin <timurovadia@gmail.com>
2020-11-09 17:08:04 +0300

    added file

---
+++ count_if_example.cpp
@@ -0,0 +1,21 @@
+﻿//Напишите функцию, которая принимает вектор строк и возвращает число строк, начинающихся на большую или маленькую букву "a".
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool IsFirstLetterA(const string& s) {
+    return !s.empty() && (s[0] == 'a' || s[0] == 'A');
+}
+
+int CountStartsWithA(const vector<string>& xs) {
+    return count_if(xs.begin(), xs.end(), IsFirstLetterA);
+}
+
+int main() {
+    cout << CountStartsWithA({ "And"s, "another"s, "one"s, "gone"s, "another"s, "one"s "bites"s, "the"s, "dust"s });
+    return 0;
+}
