39c7b341f8fa980b04bc26c28c64567b61f8ae42
Timur Berezin <timurovadia@gmail.com>
2020-11-09 22:06:08 +0300

    added file

---
+++ create_sort_vector_pair.cpp
@@ -0,0 +1,29 @@
+﻿//Считайте количество людей, имя и возраст каждого. 
+//Выведите все имена с новой строки по старшинству — от старых к молодым.
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int count;
+    cin >> count;
+    vector<pair<int, string>> v_person_data;
+    for (int i = 0; i < count; ++i) {
+        string name;
+        int age;
+        cin >> name >> age;
+        v_person_data.push_back({ age, name });
+    }
+
+    sort(v_person_data.begin(), v_person_data.end());
+    reverse(v_person_data.begin(), v_person_data.end());
+
+    for (auto& person : v_person_data) {
+        cout << person.second << endl;
+    }
+    return 0;
+}
---
+++ foo_calc_median_vector.cpp
@@ -0,0 +1,52 @@
+﻿//Напишите функцию, которая принимает на вход вектор времени ответа и вычисляет медиану этого вектора.
+//Вектор может быть неотсортированным.
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+pair<bool, double> CalcMedian(vector<double>& samples) {
+    // верните {true, медиана}, если она существует, то есть вектор непустой,
+    // иначе - {false, 0}
+    bool exist = false;
+    double median = 0;
+    sort(samples.begin(), samples.end());
+    if (samples.size() % 2 == 0 && !samples.empty()) {
+        // чётное
+        median = 0.5 * (samples[(samples.size() / 2) - 1] + samples[(samples.size() / 2)]);
+        exist = true;
+    }
+    else if (!samples.empty()) {
+        // нечётное
+        median = samples[samples.size() / 2];
+        exist = true;
+    }
+    else {
+        exist = false;
+    }
+    return { exist, median };
+}
+
+int main() {
+    int size;
+    cin >> size;
+
+    vector<double> samples;
+    for (int i = 0; i < size; ++i) {
+        double sample;
+        cin >> sample;
+        samples.push_back(sample);
+    }
+
+    pair<bool, double> result = CalcMedian(samples);
+    if (result.first) {
+        cout << result.second << endl;
+    }
+    else {
+        cout << "Empty vector"s << endl;
+    }
+
+    return 0;
+}
